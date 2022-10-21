b88efdfe31f9020d68a81ed45286d56ca5c38a8d
Jaedson Silva <contacanal798@gmail.com>
2022-10-21 20:53:43 -0300

    adding initial setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    author='Jaedson Silva',
+    author_email='imunknowuser@protonmail.com',
+    name='wsblib',
+    description='Base library for other web servers.'
+)
