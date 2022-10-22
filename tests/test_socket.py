b63e13c40e78555710fd2d602f116cb50a3aaaf3
Jaedson Silva <contacanal798@gmail.com>
2022-10-21 21:51:12 -0300

    adding test_server unit test

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -17,6 +17,15 @@ class TestSocket(bupytest.UnitTest):
 
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    def test_server(self) -> None:
+        self.client.connect(('localhost', 2808))
+        self.client.send(b'wsblib test')
+
+        client = self.server.wait_client()
+        message = client.get_message()
+
+        self.assert_expected(message, 'wsblib test')
+
 
 if __name__ == '__main__':
     bupytest.this()
