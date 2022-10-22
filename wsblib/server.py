5cb425adae51d60aa10fb877cc8016f1801dd6d6
Jaedson Silva <contacanal798@gmail.com>
2022-10-21 21:07:00 -0300

    adding Server class

---
+++ wsblib/server.py
@@ -0,0 +1,7 @@
+import socket
+
+
+class Server(object):
+    def __init__(self):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
