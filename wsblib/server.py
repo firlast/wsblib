8105e74a05a0a9f0953d3f2a9b3866d5c76fc75e
Jaedson Silva <contacanal798@gmail.com>
2022-10-21 21:08:33 -0300

    adding method to start socket listen

--- wsblib/server.py
+++ wsblib/server.py
@@ -5,3 +5,9 @@ class Server(object):
     def __init__(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def start(self, host: str, port: int) -> None:
+        address = (host, port)
+
+        self._socket.bind(address)
+        self._socket.listen(128)
