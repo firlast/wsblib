53d99f7ac896e3b1d2b4e95710e2cc9bfba9c51d
Jaedson Silva <contacanal798@gmail.com>
2022-10-23 14:24:35 -0300

    adding packages to setup script

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(
     author='Jaedson Silva',
     author_email='imunknowuser@protonmail.com',
     name='wsblib',
-    description='Base library for other web servers.'
+    description='Base library for other web servers.',
+    packages=['wsblib'],
+    install_requires=['http_pyparser==0.4.1']
 )
