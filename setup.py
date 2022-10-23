97c14b54612e9ff4cb30dea79de6f0ca9b8b1de3
Jaedson Silva <contacanal798@gmail.com>
2022-10-23 14:55:08 -0300

    adding project version in setup script

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
+from wsblib import __version__
 
 setup(
     author='Jaedson Silva',
     author_email='imunknowuser@protonmail.com',
     name='wsblib',
+    version=__version__,
     description='Base library for other web servers.',
     packages=['wsblib'],
     install_requires=['http_pyparser==0.4.1']
