d17d9d128b4cb35d49d7c045d149431a25a2fdfc
Jaedson Silva <contacanal798@gmail.com>
2022-11-06 16:00:04 -0300

    print request and response log in log_request

--- wsblib/log.py
+++ wsblib/log.py
@@ -11,3 +11,15 @@ def log_request(response: Response, request: RequestData) -> None:
     path = request.path
     method = request.method
     host = request.remote_addr[0]
+
+    print(f'[\033[33m{time}\033[m]', end='')
+    print(f'[{host}]', end=' ', flush=True)
+
+    if 100 <= status < 200 or 300 <= status < 400:
+        print(f'\033[33m{status}\033[m', flush=True, end=' ')
+    elif 200 <= status < 300:
+        print(f'\033[32m{status}\033[m', flush=True, end=' ')
+    elif 400 <= status <= 500:
+        print(f'\033[31m{status}\033[m', flush=True, end=' ')
+
+    print(f'{path} ({method})')
