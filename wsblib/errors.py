32fc027e8bb20944ca45abd06e428cf2fa9f852c
Jaedson Silva <contacanal798@gmail.com>
2022-11-07 17:29:28 -0300

    adding match_status_code in Error class

--- wsblib/errors.py
+++ wsblib/errors.py
@@ -1,7 +1,15 @@
 from types import FunctionType
 
+from .exceptions import InvalidResponseError
+from .request import RequestData
+
+from http_pyparser import response
+
 
 class Error:
     def __init__(self, callback: FunctionType, status_code: int) -> None:
         self._callback = callback
         self._status_code = status_code
+
+    def match_status_code(self, status_code: int) -> bool:
+        return self._status_code == status_code
