1c6b60ea759440ab7daac4eb729a83b4b0984757
Jaedson Silva <contacanal798@gmail.com>
2022-12-03 14:42:23 -0300

    adding SocketMock methods

--- tests/mock.py
+++ tests/mock.py
@@ -1,3 +1,18 @@
 class SocketMock:
     def __init__(self, message: str):
         self._message = message.encode()
+
+    def send(self, data) -> None:
+        pass
+
+    def settimeout(self, data) -> None:
+        pass
+
+    def recv(self, bufsize) -> bytes:
+        return self._message
+
+    def shutdown(self, data) -> None:
+        pass
+
+    def close(self) -> None:
+        pass
