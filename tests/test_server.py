175ea5089154d75b06b9535bea59a8ba2ed9cd6c
Jaedson Silva <contacanal798@gmail.com>
2022-12-03 14:50:06 -0300

    changing route 2 path

--- tests/test_server.py
+++ tests/test_server.py
@@ -7,6 +7,7 @@ from http_pyparser.parser import HTTPData
 sys.path.insert(0, './')
 
 from wsblib import route
+from wsblib import request as wsb_request
 
 
 class TestServer(bupytest.UnitTest):
@@ -17,10 +18,10 @@ class TestServer(bupytest.UnitTest):
             return 'Hello World!'
 
         def callback_2():
-            return {'status': 'created', 'id': 28}, 201
+            return {'name': 'WSBLib', 'id': 28}, 201
 
         self.index_route = route.Route(callback, '/', methods=('GET', 'POST'))
-        self.about_route = route.Route(callback_2, '/about')
+        self.about_route = route.Route(callback_2, '/wsblib')
 
     def test_route(self):
         self.assert_true(self.index_route.match_route('/'))
@@ -29,8 +30,8 @@ class TestServer(bupytest.UnitTest):
         self.assert_true(self.index_route.accept_method('GET'))
         self.assert_false(self.index_route.accept_method('PUT'))
 
-        self.assert_true(self.about_route.match_route('/about'))
-        self.assert_false(self.about_route.match_route('/sobre'))
+        self.assert_true(self.about_route.match_route('/wsblib'))
+        self.assert_false(self.about_route.match_route('/json'))
         self.assert_true(self.about_route.accept_method('GET'))
         self.assert_false(self.about_route.accept_method('POST'))
 
@@ -48,7 +49,7 @@ class TestServer(bupytest.UnitTest):
         about_response = self.about_route.get_route_response(fake_http_data)
 
         self.assert_true(isinstance(about_response, Response))
-        self.assert_expected(about_response.body, {'status': 'created', 'id': 28})
+        self.assert_expected(about_response.body, {'name': 'WSBLib', 'id': 28})
         self.assert_expected(about_response.content_type, 'text/html')
         self.assert_expected(about_response.status, 201)
 
