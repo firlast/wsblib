c54a29912749610739e0f5a8d5327bef29dc29c1
Jaedson Silva <contacanal798@gmail.com>
2022-12-03 15:08:40 -0300

    testing request process in test_process_request

--- tests/test_server.py
+++ tests/test_server.py
@@ -4,9 +4,12 @@ import bupytest
 from http_pyparser.response import Response
 from http_pyparser.parser import HTTPData
 
+from mock import SocketMock
+
 sys.path.insert(0, './')
 
 from wsblib import route
+from wsblib import server
 from wsblib import request as wsb_request
 
 
@@ -38,6 +41,25 @@ class TestServer(bupytest.UnitTest):
         self.assert_expected(index_response.body, 'Hello World!')
         self.assert_expected(index_response.content_type, 'text/html')
         self.assert_expected(index_response.status, 200)
+
+    def test_process_request(self):
+        project_route_http = ('GET /project?id=28 HTTP/1.1\r\n'
+                              'Cookie: projectId=28\r\n\r\n')
+
+        route_list = [self.index_route, self.project_route]
+        process = wsb_request.ProcessRequest(route_list)
+
+        mock_socket = SocketMock(project_route_http)
+        client = server.Client(mock_socket, ('127.0.0.1', 5500))
+        request_processed = process.process(client)
+
+        # changing query data from request data
+        request_processed.request.query = {'id': '30'}
+        response = request_processed.get_response()
+
+        self.assert_expected(response.body, {'name': 'WSBLib', 'id': 30})
+        self.assert_expected(response.content_type, 'text/html')
+        self.assert_expected(response.status, 201)
 
 
 if __name__ == '__main__':
